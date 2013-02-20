f3c9a3d9b5af027a77e04f33311446998497c62e
Robert Davey <robjdavey@gmail.com>
2013-02-20 18:47:20 +0000

    Tidy up options page.

--- options.js
+++ options.js
@@ -1,51 +1,23 @@
 function saveOptions() {
-    saveOption("uF8FF");
-    saveOption("u80");
-    saveOption("u2000");
-    saveOption("u2139");
-    saveOption("u2194");
-    saveOption("u2300");
-    saveOption("u2460");
-    saveOption("u25A0");
-    saveOption("u2600");
-    saveOption("u2700");
-    saveOption("u2900");
-    saveOption("u2B00");
-    saveOption("u3000");
-    saveOption("u3200");
-    saveOption("u1F100");
-    saveOption("u1F200");
-    saveOption("u1F300");
-    saveOption("u1F600");
-    saveOption("u1F680");
+    allCheckboxes(saveOption);
     
     var status = document.getElementById("status");
     status.innerHTML = "Options Saved.";
     setTimeout(function() {
         status.innerHTML = "&nbsp;";
-    }, 750);
+    }, 1000);
 }
 
 function loadOptions() {
-    loadOption("uF8FF");
-    loadOption("u80");
-    loadOption("u2000");
-    loadOption("u2139");
-    loadOption("u2194");
-    loadOption("u2300");
-    loadOption("u2460");
-    loadOption("u25A0");
-    loadOption("u2600");
-    loadOption("u2700");
-    loadOption("u2900");
-    loadOption("u2B00");
-    loadOption("u3000");
-    loadOption("u3200");
-    loadOption("u1F100");
-    loadOption("u1F200");
-    loadOption("u1F300");
-    loadOption("u1F600");
-    loadOption("u1F680");
+    allCheckboxes(loadOption);
+}
+
+function allCheckboxes(callback) {
+    var elements = document.getElementsByName("codepage");
+    for(var i = 0; i < elements.length; i++) {
+        var element = elements[i];
+        callback(element.id);
+    }
 }
 
 function saveOption(id) {
@@ -99,26 +71,63 @@ function setChars(id, from, to) {
     }
 }
 
-function init() {
-    setChars("0xF8FF", 0xF8FF, 0xF8FF);
-    setChars("0x80", 0x80, 0xFF);
-    setChars("0x2000", 0x2000, 0x206F);
-    setChars("0x2139", 0x2139, 0x214F);
-    setChars("0x2194", 0x2194, 0x21FF);
-    setChars("0x2300", 0x2300, 0x23FF);
-    setChars("0x2460", 0x2460, 0x24FF);
-    setChars("0x25A0", 0x25A0, 0x25FF);
-    setChars("0x2600", 0x2600, 0x26FF);
-    setChars("0x2700", 0x2700, 0x27BF);
-    setChars("0x2900", 0x2900, 0x29FF);
-    setChars("0x2B00", 0x2B00, 0x2BFF);
-    setChars("0x3000", 0x3000, 0x303F);
-    setChars("0x3200", 0x3200, 0x32FF);
-    setChars("0x1F100", 0x1F100, 0x1F1FF);
-    setChars("0x1F200", 0x1F200, 0x1F2FF);
+function init() {    
+    //Apple logo
+	setChars("0xF8FF", 0xF8FF, 0xF8FF);
+
+	// Latin-1 Supplement
+    setChars("0x80", 0xA9, 0xAE);
+    
+    // General Punctuation
+    setChars("0x2000", 0x203C, 0x2049);
+    
+    // Letterlike Symbols
+    setChars("0x2139", 0x2139, 0x2139);
+    
+    // Arrows
+    setChars("0x2194", 0x2194, 0x21AA);
+    
+    // Miscellaneous Technical
+    setChars("0x2300", 0x231A, 0x23F3);
+    
+    // Enclosed Alphanumerics
+    setChars("0x2460", 0x24C2, 0x24C2);
+    
+    // Geometric Shapes
+    setChars("0x25A0", 0x25AA, 0x25FE);
+    
+    // Miscellaneous Symbols
+    setChars("0x2600", 0x2600, 0x26FD);
+    
+    // Dingbats
+    setChars("0x2700", 0x2702, 0x27BF);
+    
+    // Supplemental Arrows-B
+    setChars("0x2900", 0x2934, 0x2935);
+    
+    // Miscellaneous Symbols and Arrows
+    setChars("0x2B00", 0x2B05, 0x2B55);
+    
+    // CJK Symbols and Punctuation
+    setChars("0x3000", 0x3030, 0x303D);
+    
+    // Enclosed CJK Letters and Months
+    setChars("0x3200", 0x3297, 0x3299);
+    
+    // Enclosed Alphanumeric Supplement
+    setChars("0x1F100", 0x1F170, 0x1F19A);
+    
+    // Enclosed Ideographic Supplement
+    setChars("0x1F200", 0x1F201, 0x1F251);
+    
+    // Miscellaneous Symbols and Pictographs
     setChars("0x1F300", 0x1F300, 0x1F5FF);
+    
+    // Emoticons
     setChars("0x1F600", 0x1F600, 0x1F64F);
-    setChars("0x1F680", 0x1F680, 0x1F6FF);
+    
+    // Transport and Map Symbols
+    setChars("0x1F680", 0x1F680, 0x1F6C5);
     
     loadOptions();
 }
