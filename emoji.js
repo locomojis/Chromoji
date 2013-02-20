4aa1e4495c9437ee636c5512a182bd65d2f00442
Robert Davey <robjdavey@gmail.com>
2013-02-20 08:27:05 +0000

    Version 1.0.2
    Added support for pages that update.
    Options added.
    Jquery now used for replacing.

--- emoji.js
+++ emoji.js
@@ -44,6 +44,16 @@ function convertCharFromUtf32 (i) {
 	return s;
 }
 
+function convertStrToUtf32 (s) {
+	var hi = s.charCodeAt(0);
+	var lo = s.charCodeAt(1);
+	var X = (hi & ((1 << 6) -1)) << 10 | lo & ((1 << 10) -1);
+	var W = (hi >> 6) & ((1 << 5) - 1);
+	var U = W + 1;
+	var C = U << 16 | X;
+	return C;
+}
+
 function getHighSurrogate(i) {
 	var vd = i - 0x10000;
 	var vh = vd >> 10;
@@ -181,9 +191,9 @@ function createSearchPattern(from, to) {
 		var lowMin = surrogatesMinMax.lowMin;
 		var highMax = surrogatesMinMax.highMax;
 		var lowMax = surrogatesMinMax.lowMax;
-		pattern = "[" + getAsUtf16(highMin) + "-" + getAsUtf16(highMax) + "][" + getAsUtf16(lowMin) + "-" + getAsUtf16(lowMax) + "]";
+		pattern = "([" + getAsUtf16(highMin) + "-" + getAsUtf16(highMax) + "][" + getAsUtf16(lowMin) + "-" + getAsUtf16(lowMax) + "])";
 	} else {
-		pattern = "[" + getAsUtf16(from) + "-" + getAsUtf16(to) + "]";
+		pattern = "([" + getAsUtf16(from) + "-" + getAsUtf16(to) + "])";
 	}
 	return pattern;
 }
@@ -205,9 +215,18 @@ function checkForCharsMulti(fromFirst, toFirst, fromSecond, toSecond, s) {
 
 function testAndReplace(from, to, s) {
 	if(checkForChars(from, to, s)) {
-		for(var i = from; i <= to; i++) {
-			s = replace(i, s);
-		}
+		var pattern = createSearchPattern(from, to);
+		var regexp = new RegExp(pattern, 'g');
+		s = s.replace(regexp, function(a) {
+			var i = convertStrToUtf32(a);
+			var path = "images/" + getHexString(i) + ".png";
+			var image = chrome.extension.getURL(path);
+			if(fileExists(image)) {
+				console.log(path);
+				return createReplacementString(i, image);
+			}
+			return s;
+		});
 	}
 	return s;
 }
@@ -259,67 +278,193 @@ function replaceSpecificChars(s) {
 	return s;
 }
 
-function run() {
-	var body = document.body.innerHTML;
-	
-    // Multi & indidual chars
-	body = replaceSpecificChars(body);
-    
-    // Latin-1 Supplement
-    body = testAndReplace(0x80, 0xFF, body);
-    
-    // General Punctuation
-    body = testAndReplace(0x2000, 0x206F, body);
-    
-    // Letterlike Symbols
-    body = testAndReplace(0x2139, 0x214F, body);
-    
-    // Arrows
-    body = testAndReplace(0x2194, 0x21FF, body);
-    
-    // Miscellaneous Technical
-    body = testAndReplace(0x2300, 0x23FF, body);
-    
-    // Enclosed Alphanumerics
-    body = testAndReplace(0x2460, 0x24FF, body);
-    
-    // Geometric Shapes
-    body = testAndReplace(0x25A0, 0x25FF, body);
-    
-    // Miscellaneous Symbols
-    body = testAndReplace(0x2600, 0x26FF, body);
-    
-    // Dingbats
-    body = testAndReplace(0x2700, 0x27BF, body);
-    
-    // Supplemental Arrows-B
-    body = testAndReplace(0x2900, 0x29FF, body);
-    
-    // Miscellaneous Symbols and Arrows
-    body = testAndReplace(0x2B00, 0x2BFF, body);
-    
-    // CJK Symbols and Punctuation
-    body = testAndReplace(0x3000, 0x303F, body);
-    
-    // Enclosed CJK Letters and Months
-    body = testAndReplace(0x3200, 0x32FF, body);
-    
-    // Enclosed Alphanumeric Supplement
-    body = testAndReplace(0x1F100, 0x1F1FF, body);
-    
-    // Enclosed Ideographic Supplement
-    body = testAndReplace(0x1F200, 0x1F2FF, body);
-    
-    // Miscellaneous Symbols and Pictographs
-    body = testAndReplace(0x1F300, 0x1F5FF, body);
-    
-    // Emoticons
-	body = testAndReplace(0x1F600, 0x1F64F, body);
-    
-    // Transport and Map Symbols
-	body = testAndReplace(0x1F680, 0x1F6FF, body);
-	
-	document.body.innerHTML = body;
+jQuery.fn.justtext = function() {
+   
+    return $(this)  .clone()
+            .children()
+            .remove()
+            .end()
+            .text();
+ 
+};
+
+function doReplace(id, from, to)
+{
+	if(settings[id]) {
+		var pattern = createSearchPattern(from, to);
+		var regexp = new RegExp(pattern, 'g');
+		var nodes = $('body').find('*').filter(function(index) {
+		
+			var contents = regexp.test($(this).justtext());
+			return contents;
+		});
+		nodes.each(function(i, v) {
+			$(this).html($(this).html().replace(regexp,
+				function(a) {
+					var i;
+					if(a.length == 2) {
+						i = convertStrToUtf32(a);
+					} else {
+						i = a.charCodeAt(0);
+					}
+					var path = "images/" + getHexString(i) + ".png";
+					var image = chrome.extension.getURL(path);
+					if(fileExists(image)) {
+						return createReplacementString(i, image);
+					}
+					return a;
+				}
+			))
+		});
+	}
 }
 
-run();
+function getLocalStorageVal(id, from, to) {
+    chrome.extension.sendMessage({greeting: id}, function(response) {
+        var res = response.farewell;
+		settings[id] = (res == "true" || res == "True" || res == "TRUE");
+    });
+}
+
+function init() {	
+    // Multi & indidual chars
+	//body = replaceSpecificChars(body);
+    
+    // Latin-1 Supplement
+    getLocalStorageVal("u80", 0x80, 0xFF);
+    
+    // General Punctuation
+    getLocalStorageVal("u2000", 0x2000, 0x206F);
+    
+    // Letterlike Symbols
+    getLocalStorageVal("u2139", 0x2139, 0x214F);
+    
+    // Arrows
+    getLocalStorageVal("u2194", 0x2194, 0x21FF);
+    
+    // Miscellaneous Technical
+    getLocalStorageVal("u2300", 0x2300, 0x23FF);
+    
+    // Enclosed Alphanumerics
+    getLocalStorageVal("u2460", 0x2460, 0x24FF);
+    
+    // Geometric Shapes
+    getLocalStorageVal("u25A0", 0x25A0, 0x25FF);
+    
+    // Miscellaneous Symbols
+    getLocalStorageVal("u2600", 0x2600, 0x26FF);
+    
+    // Dingbats
+    getLocalStorageVal("u2700", 0x2700, 0x27BF);
+    
+    // Supplemental Arrows-B
+    getLocalStorageVal("u2900", 0x2900, 0x29FF);
+    
+    // Miscellaneous Symbols and Arrows
+    getLocalStorageVal("u2B00", 0x2B00, 0x2BFF);
+    
+    // CJK Symbols and Punctuation
+    getLocalStorageVal("u3000", 0x3000, 0x303F);
+    
+    // Enclosed CJK Letters and Months
+    getLocalStorageVal("u3200", 0x3200, 0x32FF);
+    
+    // Enclosed Alphanumeric Supplement
+    getLocalStorageVal("u1F100", 0x1F100, 0x1F1FF);
+    
+    // Enclosed Ideographic Supplement
+    getLocalStorageVal("u1F200", 0x1F200, 0x1F2FF);
+    
+    // Miscellaneous Symbols and Pictographs
+    getLocalStorageVal("u1F300", 0x1F300, 0x1F5FF);
+    
+    // Emoticons
+    getLocalStorageVal("u1F600", 0x1F600, 0x1F64F);
+    
+    // Transport and Map Symbols
+    getLocalStorageVal("u1F680", 0x1F680, 0x1F6FF);
+	
+	onDOMChanged('body', DOMChangedEventHandler, 1000);
+}
+
+function run() {
+	// Latin-1 Supplement
+    doReplace("u80", 0x80, 0xFF);
+    
+    // General Punctuation
+    doReplace("u2000", 0x2000, 0x206F);
+    
+    // Letterlike Symbols
+    doReplace("u2139", 0x2139, 0x214F);
+    
+    // Arrows
+    doReplace("u2194", 0x2194, 0x21FF);
+    
+    // Miscellaneous Technical
+    doReplace("u2300", 0x2300, 0x23FF);
+    
+    // Enclosed Alphanumerics
+    doReplace("u2460", 0x2460, 0x24FF);
+    
+    // Geometric Shapes
+    doReplace("u25A0", 0x25A0, 0x25FF);
+    
+    // Miscellaneous Symbols
+    doReplace("u2600", 0x2600, 0x26FF);
+    
+    // Dingbats
+    doReplace("u2700", 0x2700, 0x27BF);
+    
+    // Supplemental Arrows-B
+    doReplace("u2900", 0x2900, 0x29FF);
+    
+    // Miscellaneous Symbols and Arrows
+    doReplace("u2B00", 0x2B00, 0x2BFF);
+    
+    // CJK Symbols and Punctuation
+    doReplace("u3000", 0x3000, 0x303F);
+    
+    // Enclosed CJK Letters and Months
+    doReplace("u3200", 0x3200, 0x32FF);
+    
+    // Enclosed Alphanumeric Supplement
+    doReplace("u1F100", 0x1F100, 0x1F1FF);
+    
+    // Enclosed Ideographic Supplement
+    doReplace("u1F200", 0x1F200, 0x1F2FF);
+    
+    // Miscellaneous Symbols and Pictographs
+    doReplace("u1F300", 0x1F300, 0x1F5FF);
+    
+    // Emoticons
+    doReplace("u1F600", 0x1F600, 0x1F64F);
+    
+    // Transport and Map Symbols
+    doReplace("u1F680", 0x1F680, 0x1F6FF);
+}
+
+function DOMChangedEventHandler () {
+    run();
+}
+
+function onDOMChanged(selector, actionFunction, delay)
+{
+    $(selector).bind ('DOMSubtreeModified', fireOnDelay);
+
+    function fireOnDelay () {
+        if (typeof this.domTimer == "number") {
+            clearTimeout (this.domTimer);
+        }
+        
+        this.domTimer  = setTimeout(function() { onTimer (); }, delay);
+    }
+
+    function onTimer () {
+        $(selector).unbind ('DOMSubtreeModified', fireOnDelay);
+        actionFunction ();
+        $(selector).bind ('DOMSubtreeModified', fireOnDelay);
+    }
+}
+
+var settings = new Object();
+init();
