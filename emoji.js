09a7888a1a6e645238cf21f0cc604f0654c0fc3d
Robert Davey <robjdavey@gmail.com>
2013-03-09 17:31:36 +0000

    Completed support for multiple chars per emoji
    This commit fixes #8 and starts to encode the SoftBank encoding required
    for #9.
    The options page has been completely removed as it no longer seems
    necessary due to increased performance.

--- emoji.js
+++ emoji.js
@@ -1,361 +1,19 @@
-﻿var BMP_MAX = 0xFFFF;
-var ASCII_MAX = 0xFF;
-
-function createReplacementString(i, image) {
-	var message = getMessage(i);
-	var replacement = "<img src='" + image + "' class='emoji' ";
-    if(message.length > 0)
-    {
-        replacement += "alt='" + message + "' title='" + message + "' ";
-    }
-    replacement += "/>";
-	return replacement;
-}
-
-function getMinAndMaxSurrogates(from, to) {
-	var highMin, lowMin, highMax, lowMax;
-		
-	for(var i = from; i <= to; i++) {
-		var low = getLowSurrogate(i);
-		var high = getHighSurrogate(i);
-		
-		if(i == from) {
-			highMin = high;
-			highMax = high;
-			lowMin = low;
-			lowMax = low;
-		} else {
-			highMin = Math.min(high, highMin);
-			highMax = Math.max(high, highMax);
-			lowMin = Math.min(low, lowMin);
-			lowMax = Math.max(low, lowMax);
-		}
-	}
-	
-	var result = new Object();
-	result.highMin = highMin;
-	result.highMax = highMax;
-	result.lowMin = lowMin;
-	result.lowMax = lowMax;
-	return result;
-}
-
-function createSearchPattern(from, to) {
-	var pattern;
-	if(from > BMP_MAX) {
-		var surrogatesMinMax = 	getMinAndMaxSurrogates(from, to);
-		var highMin = surrogatesMinMax.highMin;
-		var lowMin = surrogatesMinMax.lowMin;
-		var highMax = surrogatesMinMax.highMax;
-		var lowMax = surrogatesMinMax.lowMax;
-		pattern = "[" + getAsUtf16(highMin) + "-" + getAsUtf16(highMax) + "][" + getAsUtf16(lowMin) + "-" + getAsUtf16(lowMax) + "]";
-	} else {
-		pattern = "[" + getAsUtf16(from) + "-" + getAsUtf16(to) + "]";
-	}
-	return pattern;
-}
-
-jQuery.fn.justtext = function() {
+﻿jQuery.fn.just_text = function() {
     return $(this).clone()
-            .children()
-            .remove()
-            .end()
-            .text();
- 
-};
-
-function getMultibyteHexString(s) {
-    var result = "";
-    for(var i = 0; i < s.length; i++) {
-        var c1 = s.charCodeAt(i);
-        if(c1 >= 0xD800 && c1 <= 0xDB7F) {
-            // High surrogate
-            var h = s[i] + s[i + 1];
-            var c = convertStrToUtf32(h);
-            result += getHexString(c);
-            i++;
-        } else {
-            result += getHexString(c1);
-        }
-        result += "-";
-    }
-    
-    if(result != "") {
-        result = result.substr(0, result.length - 1);
-    }
-    
-    return result;
+        .children()
+        .remove()
+        .end()
+        .text();
 }
 
-function doReplaceNodes(regexp, nodes) {
-    $.each(nodes, function(i, v) {
-        var node = $(this);
-        if(node && node.html()) {
-            node.html(node.html().replace(regexp, 
-                function(a) {
-                    var hex;
-                    if(a.length >= 2) {
-                        hex = getMultibyteHexString(a);
-                    } else {
-                        hex = getHexString(a.charCodeAt(0));
-                    }
-                    console.log(node.html());
-                    var replacement = replacements[hex];
-                    if(replacement) {
-                        return replacement;
-                    } else {
-                        return a;
-                    }
-                })
-            );
-        }
-    });
-}
-
-function doReplace(id, from, to)
-{
-	if(settings[id]) {
-		var pattern = createSearchPattern(from, to);
-		var regexp = new RegExp(pattern, 'g');
-		var nodes = getBodyNodes(regexp);
-		doReplaceNodes(regexp, nodes);
-	}
-}
-
-function processImageCacheResponse(response) {
-    responses++;
-    var image = response.result;
-    var character = response.character
-    if(image != "") {
-        replacement = createReplacementString(character, image);
-        replacements[character] = replacement;
-    }
-
-    if(requests == responses) {
-        run();
-    }
-}
-
-function processLocalStorageResponse(response) {
-    responses++;
-    var id = response.setting;
-    var res = response.result;
-    settings[id] = (res == "true" || res == "True" || res == "TRUE");
-    if(settings[id]) {
-        var from = response.from;
-        var to = response.to;
-        var chars = response.chars;
-        var items = response.items;
-        if(from && to) {
-            for(var i = from; i <= to; i++) {
-                var s = getHexString(i);
-                var replacement = replacements[s];
-                if(!replacement) {
-                    requests++;
-                    chrome.extension.sendMessage({character: s}, processImageCacheResponse);
-                }
-            }
-        } else if (chars) {
-            for(var i = 0; i < chars.length; i++) {
-                var val = parseInt(chars[i]);
-                var s = getHexString(val);
-                requests++;
-                chrome.extension.sendMessage({character: s}, processImageCacheResponse);
-            }
-        } else if (items) {
-            for(var i = 0; i < items.length; i++) {
-                var item = items[i];
-                var chars = item.chars;
-                var s = getMultiCharName(chars);
-                requests++;
-                chrome.extension.sendMessage({character: s}, processImageCacheResponse);
-            }
-        }
-    }
-}
-
-function getLocalStorageForBlock(id, from, to) {
-	requests++;
-    chrome.extension.sendMessage({setting: id, from: from, to: to}, processLocalStorageResponse);
-}
-
-function getLocalStorageForSingle(id, chars) {
-    requests++;
-    chrome.extension.sendMessage({setting: id, chars: chars}, processLocalStorageResponse);
-}
-
-function getLocalStorageForMulti(id, items) {
-    requests++;
-    chrome.extension.sendMessage({setting: id, items: items}, processLocalStorageResponse);
-}
-
-var requests = 0;
-var responses = 0;
-
-function init() {	
-    getCharBlocks(function(result) {
-        blocks = result;
-		for(var i = 0; i < blocks.length; i++) {
-			var block = blocks[i];
-			var start = parseInt(block.block_start);
-			var from = parseInt(block.char_start);
-			var to = parseInt(block.char_end);
-			var id = block.id;
-			getLocalStorageForBlock(id, from, to);
-		}
-	});
-    
-    getSingles(function(result) {
-        singles = result;
-        for(var i = 0; i < singles.length; i++) {
-            var single = singles[i];
-            var chars = single.chars;
-            var id = single.id;
-            getLocalStorageForSingle(id, chars);
-        }
-    });
-    
-    getMultis(function(result) {
-        multis = result;
-        for(var i = 0; i < multis.length; i++) {
-            var multi = multis[i];
-            var id = multi.id;
-            var items = multi.items;
-            getLocalStorageForMulti(id, items);
-        }
-    });
-}
-
-function getBodyNodes(regexp) {
-    return filterNodes(document.body, regexp);
-}
-
-function filterNodes(nodes, regexp) {
+function filter_nodes(nodes, regexp) {
     return $(nodes).find('[contenteditable!="true"][contenteditable!="plaintext-only"]').filter(
         function(index) {
-            var text = $(this).justtext();
-			var contents = (text.search(regexp) != 0)
-			return contents;
+			var text = $(this).just_text();
+            var result = (text.search(regexp) != -1)
+            return result;
 		}
     );
-}
-
-function createSinglesPattern(singles) {
-    var pattern = "";
-    
-    for(var i = 0; i < singles.length; i++) {
-        var single = singles[i];
-        var id = single.id;
-        if(settings[id]) {
-            var chars = single.chars;
-            for(var j = 0; j < chars.length; j++) {
-                var c = parseInt(chars[j]);
-                var s = getAsUtf16(c);
-                pattern += (s + "|");
-            }
-        }
-    }
-    
-    if(pattern != "") {
-        pattern = pattern.substr(0, pattern.length - 1);
-    }
-    
-    return pattern;
-}
-
-function createMultisPattern(multis) {
-    var pattern = "";
-    
-    for(var i = 0; i < multis.length; i++) {
-        var multi = multis[i];
-        var id = multi.id;
-        if(settings[id]) {
-            var items = multi.items;
-            for(var j = 0; j < items.length; j++) {
-                var item = items[j];
-                var chars = item.chars;
-                for(var k = 0; k < chars.length; k++) {
-                    var c = parseInt(chars[k]);
-                    var s = getAsUnicode(c);
-                    pattern += s;
-                }
-                pattern += "|";
-            }
-        }
-    }
-    
-    if(pattern != "") {
-        pattern = pattern.substr(0, pattern.length - 1);
-    }
-    
-    return pattern;
-}
-
-function createBlocksPattern(blocks) {
-    var pattern = "";
-    
-    for(var i = 0; i < blocks.length; i++) {
-        var block = blocks[i];
-        var id = block.id;
-        if(settings[id]) {
-            var from = parseInt(block.char_start);
-            var to = parseInt(block.char_end);
-            pattern += createSearchPattern(from, to);
-            pattern += "|";
-        }
-    }
-    
-    if(pattern != "") {
-        pattern = pattern.substr(0, pattern.length - 1);
-    }
-    
-    return pattern;
-}
-
-function run(nodes) {
-    var regexp;
-    
-    if(blocks) {
-        var pattern = createBlocksPattern(blocks);
-		if(pattern != "") {
-            regexp = new RegExp(pattern, 'g');
-            if(!nodes) {
-                var target = getBodyNodes(regexp);
-                doReplaceNodes(regexp, target);
-            } else {
-                var target = filterNodes(nodes, regexp);
-                doReplaceNodes(regexp, nodes);
-            }
-        }
-    }
-    
-    if(singles) {
-        var pattern = createSinglesPattern(singles);
-        if(pattern != "") {
-            regexp = new RegExp(pattern, 'g');
-            if(!nodes) {
-                var target = getBodyNodes(regexp);
-                doReplaceNodes(regexp, target);
-            } else {
-                var target = filterNodes(nodes, regexp);
-                doReplaceNodes(regexp, nodes);
-            }
-        }
-    }
-    
-    if(multis) {
-        var pattern = createMultisPattern(multis);
-        if(pattern != "") {
-            regexp = new RegExp(pattern, 'g');
-            if(!nodes) {
-                var target = getBodyNodes(regexp);
-                doReplaceNodes(regexp, target);
-            } else {
-                var target = filterNodes(nodes, regexp);
-                doReplaceNodes(regexp, nodes);
-            }
-        }
-    }
 }
 
 function on_mutation(mutations) {
@@ -364,49 +22,97 @@ function on_mutation(mutations) {
         var added = mutation.addedNodes;
         
         if(added.length > 0) {
-            if(blocks) {
-                var pattern = createBlocksPattern(blocks);
-                if(pattern != "") {
-                    var regexp = new RegExp(pattern, 'g');
-                    var target = filterNodes(added, regexp);
-                    doReplaceNodes(regexp, target);
-                }
-            }
-            
-            if (singles) {
-                var pattern = createSinglesPattern(singles);
-                if(pattern != "") {
-                    var regexp = new RegExp(pattern, 'g');
-                    var target = filterNodes(added, regexp);
-                    doReplaceNodes(regexp, target);
-                }
-            }
-            
-            if(multis) {
-                var pattern = createMultisPattern(multis);
-                if(pattern != "") {
-                    regexp = new RegExp(pattern, 'g');                    
-                    var target = filterNodes(added, regexp);
-                    doReplaceNodes(regexp, target);
-                }
-            }
+            var nodes = filter_nodes(added, regexp);
+            run(nodes);
         }
     }
 }
 
-var blocks;
-var singles;
-var multis;
-var settings = new Object();
-var replacements = new Object();
-var target = document.body;
-var config = { childList: true, characterData: true, subtree: true };
-var observer = new WebKitMutationObserver(on_mutation);
-observer.observe(target, config);
+function get_replacement(image) {
+    var relative = "images/" + image;
+    var absolute = chrome.extension.getURL(relative);
+    var element = "<img src='" + absolute + "' class='emoji' />";
+    return element;
+}
+
+function run(nodes) {
+    $.each(nodes,
+        function() {
+            $(this).html($(this).html().replace(regexp,
+                function(c) {
+                    var matched = valid.filter(
+                        function(element, index, array) {
+                            if(element.chars.indexOf(c) != -1) {
+                                return element.image;
+                            }
+                        }
+                    );
+                    
+                    if(matched.length > 0) {
+                        var image = matched[0].image
+                        return get_replacement(image);
+                    }
+                    
+                    return c;
+                }
+            ));
+        }
+    );
+}
+
+function start_observer() {
+    var target = document.body;
+    var config = { childList: true, characterData: true, subtree: true };
+    var observer = new WebKitMutationObserver(on_mutation);
+    observer.observe(target, config);
+}
+
+function create_pattern(items) {
+    pattern = "";
+    items.forEach(
+        function (element, index, array) {
+            var chars = element.chars;
+            chars.forEach(
+                function (element, index, array) {
+                    pattern += (element + "|");
+                }
+            );
+        }
+    );
+
+    if (pattern != "") {
+        pattern = pattern.substr(0, pattern.length - 1);
+    }
+}
+
+function init() {
+    readCharDictionary(
+        function (chars) {
+            charDictionary = chars
+            items = chars.items;
+            valid = items.filter(
+                function (element, index, array) {
+                    return (element.image != "");
+                }
+            );
+
+            create_pattern(valid);
+            regexp = new RegExp(pattern, 'g');
+            var nodes = filter_nodes($('body'), regexp);
+            run(nodes);
+            start_observer();
+        }
+    );
+}
+
+var charDictionary;
+var items;
+var valid;
+var pattern;
+var regexp;
 
 $(document).ready(
-	function() {
-		init();
-	}
+    function () {
+        init();
+    }
 );
-
--- shared.js
+++ shared.js
@@ -1,56 +1,4 @@
-function getHexString(i, addPadding) {
-	var str = i.toString(16);
-	var fullPad = "0000";
-	var minLen = fullPad.length;
-	if(addPadding && str.length < minLen) {
-		var len = str.length;
-		var padLen = minLen - len;
-		var pad = fullPad.substr(0, padLen);
-		str = pad + str;
-	}
-	return str;
-}
-
-function getMultiCharName(chars) {
-    if(chars.length == 0) {
-        return "";
-    }
-
-    var name = "";
-    for(var i = 0; i < chars.length; i++) {
-        var n = parseInt(chars[i]);
-        var s = n.toString(16);
-        name += (s + "-");
-    }
-    name = name.substr(0, name.length - 1);
-    return name;
-}
-
-function getMessage(i) {
-	var messageName = getHexString(i);
-	var message = chrome.i18n.getMessage(messageName);
-	return message;
-}
-
-function getUnicode(i) {
-    return i.toString(16).toUpperCase();
-}
-
-function getAsUtf16(i) {
-	return "\\u" + getHexString(i, true);
-}
-
-function getImageUrl(i) {
-	var hex = getHexString(i);
-	return getImageUrlFromString(hex);
-}
-
-function getImageUrlFromString(str) {
-    var path = "images/" + str + ".png";
-	return chrome.extension.getURL(path);
-}
-
-function readCharDictionary(callback) {
+﻿function readCharDictionary(callback) {
     var request = new XMLHttpRequest();
 	var path = "chardict.json"
 	var url = chrome.extension.getURL(path);
@@ -60,18 +8,6 @@ function readCharDictionary(callback) {
 		callback(chars);
 	}
 	request.send(null);
-}
-
-function getCharBlocks(callback) {
-	readChars(function(c) { callback(c.blocks); });
-}
-
-function getSingles(callback) {
-	readChars(function(c) { callback(c.singles); });
-}
-
-function getMultis(callback) {
-	readChars(function(c) { callback(c.multis); });
 }
 
 function getVersion(callback) {
@@ -93,50 +29,4 @@ function fileExists(path){
 	} catch(e) {
 		return false;
 	}
-}
-
-function getAsUnicode(i) {
-    if(i > 0xFFFF) {
-        var high = getHexString(getHighSurrogate(i), true);
-        var low = getHexString(getLowSurrogate(i), true);
-        return "\\u" + high + "\\u" + low;
-    } else if (i > 0xFF) {
-        return "\\u" + getHexString(i, true);
-    } else {
-        return String.fromCharCode(i);
-    }
-}
-
-function convertCharFromUtf32 (i) {
-	var vd = i - 0x10000;
-	var vh = vd >> 10;
-	var vl = vd & 0x3FF;
-	var w1 = 0xD800 + vh;
-	var w2 = 0xDC00 + vl;
-    var s = getAsUtf16(w1) + getAsUtf16(w2);
-	return s;
-}
-
-function convertStrToUtf32 (s) {
-	var hi = s.charCodeAt(0);
-	var lo = s.charCodeAt(1);
-	var X = (hi & ((1 << 6) -1)) << 10 | lo & ((1 << 10) -1);
-	var W = (hi >> 6) & ((1 << 5) - 1);
-	var U = W + 1;
-	var C = U << 16 | X;
-	return C;
-}
-
-function getHighSurrogate(i) {
-	var vd = i - 0x10000;
-	var vh = vd >> 10;
-	var w = 0xD800 + vh;
-	return w;
-}
-
-function getLowSurrogate(i) {
-	var vd = i - 0x10000;
-	var vl = vd & 0x3FF;
-	var w = 0xDC00 + vl;
-	return w;
 }
